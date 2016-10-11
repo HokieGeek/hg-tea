b798bf09e9bad10da4fbece127520cb32810a938
HokieGeek <andres.f.perez@gmail.com>
2016-10-11 16:28:36 -0400

    Adding references to AppComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';  // Needed because this will run in a browser
 
+import { AppComponent }   from './app.component' // Obivously, this is app.component.ts
+
 @NgModule({
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ], // AppComponent is the name of the class in app.component.ts
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
