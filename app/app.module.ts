57ad3331fdbe88a67230bf664b081235613e27bb
HokieGeek <andres.f.perez@gmail.com>
2016-10-16 23:54:34 -0400

    Cleanining up the module file

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';  // Needed because this will run in a browser
-import { HttpModule }  from '@angular/http';
+import { HttpModule }     from '@angular/http';
 
-import { HgTea }   from './app.hgtea'
-import { HgTeaJournal }   from './app.hgtea-journal'
-import { HgTeaJournalEntry }   from './app.hgtea-journal-entry'
+import { HgTea }              from './app.hgtea'
+import { HgTeaJournal }       from './app.hgtea-journal'
+import { HgTeaJournalEntry }  from './app.hgtea-journal-entry'
+
 import { NaturalLanguageDatePipe } from './natural-language-date-pipe'
 
 @NgModule({
   imports:      [ BrowserModule,
-                  HttpModule
+                  HttpModule,
   ],
   declarations: [ HgTea,
                   HgTeaJournal,
