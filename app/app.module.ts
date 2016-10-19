cd75127b49e171470889debbfc6635ebf4be76ee
HokieGeek <andres.f.perez@gmail.com>
2016-10-18 23:55:30 -0400

    I greatly prefer this naming scheme

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';  // Needed because this will run in a browser
 import { HttpModule }     from '@angular/http';
 
-import { HgTea }              from './app.hgtea'
-import { HgTeaJournal }       from './app.hgtea-journal'
-import { HgTeaJournalEntry }  from './app.hgtea-journal-entry'
-import { HgTeaDatabase }       from './app.hgtea-database'
-import { HgTeaDatabaseEntry }  from './app.hgtea-database-entry'
+import { HgTea }              from './hgtea.component'
+import { HgTeaJournal }       from './hgtea-journal.component'
+import { HgTeaJournalEntry }  from './hgtea-journal-entry.component'
+import { HgTeaDatabase }       from './hgtea-database.component'
+import { HgTeaDatabaseEntry }  from './hgtea-database-entry.component'
 
 import { NaturalLanguageDatePipe } from './natural-language-date-pipe'
 
