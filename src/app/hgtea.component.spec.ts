13e96d84cc570e06db1bf5036da0e7d97d85bfd9
HokieGeek <andres.f.perez@gmail.com>
2018-04-16 21:14:14 -0400

    Fixed unit tests which were always broken, it turns out

--- src/app/hgtea.component.spec.ts
+++ src/app/hgtea.component.spec.ts
@@ -1,31 +1,31 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { HgTeaComponent } from './hgtea.component';
+import { HgTea } from './hgtea.component';
 
 describe('App: Ngtest', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        HgTeaComponent
+        HgTea
       ],
     });
   });
 
   it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(HgTeaComponent);
+    let fixture = TestBed.createComponent(HgTea);
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   // it(`should have as title 'app works!'`, async(() => {
-  //   let fixture = TestBed.createComponent(HgTeaComponent);
+  //   let fixture = TestBed.createComponent(HgTea);
   //   let app = fixture.debugElement.componentInstance;
   //   expect(app.title).toEqual('app works!');
   // }));
 
   // it('should render title in a h1 tag', async(() => {
-  //   let fixture = TestBed.createComponent(HgTeaComponent);
+  //   let fixture = TestBed.createComponent(HgTea);
   //   fixture.detectChanges();
   //   let compiled = fixture.debugElement.nativeElement;
   //   expect(compiled.querySelector('h1').textContent).toContain('app works!');
