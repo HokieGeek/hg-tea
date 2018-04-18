381420957b7471462062fce0e6d5b29f979fffa5
HokieGeek <andres.f.perez@gmail.com>
2018-04-18 06:33:01 -0400

    Moved the rating teacup to its own component for readability's sake

---
+++ src/app/journal/teacupimg/teacupimg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'teacupimg',
+  templateUrl: './teacupimg.component.html',
+  styleUrls: ['./teacupimg.component.css']
+})
+export class TeacupimgComponent implements OnInit {
+    @Input() unselect: boolean
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
