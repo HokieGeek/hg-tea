3c8ee028018fcf869a0a49cd6fb53c5e21e63d78
HokieGeek <andres.f.perez@gmail.com>
2018-04-17 22:55:46 -0400

    Fixed the first of a crapton of linting errors

--- src/app/entry.ts
+++ src/app/entry.ts
@@ -1,16 +1,17 @@
 /*
 var TeaProductRatings = ["Value", "Leaf Aroma", "Brewed Aroma"];
 var TeaFlushTypes = [ ["Spring", "Summer", "Fall", "Winter"],
-                      ["1st Flush", "2nd Flush", "Monsoon Flush", "Autumn Flush"] ];                                                                                                                   kar TeaFlushTypes_Std = 0;
+                      ["1st Flush", "2nd Flush", "Monsoon Flush", "Autumn Flush"] ];
+var TeaFlushTypes_Std = 0;
 var TeaFlushTypes_Indian = 1;
 var TeaPackagingTypes = ["Loose Leaf", "Bagged", "Tuo", "Beeng", "Brick", "Mushroom", "Square"];
 */
-enum TeaFixins {"Milk", "Cream", "Half & Half",
-                "Sugar", "Brown Sugar", "Raw Sugar",
-                "Honey", "Vanilla Extract", "Vanilla Bean",
-                "Maple Cream", "Maple Sugar", "Chai Goop"};
-enum SteepingVessels {"French Press", "Shipiao Yixing", "Tea-zer Tumbler",
-                      "Tea Stick", "Mesh Spoon", "Sauce Pan",
+enum TeaFixins {'Milk', 'Cream', 'Half & Half',
+                'Sugar', 'Brown Sugar', 'Raw Sugar',
+                'Honey', 'Vanilla Extract', 'Vanilla Bean',
+                'Maple Cream', 'Maple Sugar', 'Chai Goop'};
+enum SteepingVessels {'French Press', 'Shipiao Yixing', 'Tea-zer Tumbler',
+                      'Tea Stick', 'Mesh Spoon', 'Sauce Pan',
                       Cup, Bowl, Gaiwan, Other};
 
 export class Entry {
@@ -32,25 +33,25 @@ export class Entry {
     get steepingvessel() { return SteepingVessels[this.steepingvessel_idx]; }
     get fixins() {
         if (this.fixins_list.length > 0) {
-            var fixins_str = "";
-            for (let f of this.fixins_list.split(";").map(f => TeaFixins[f])) {
-                fixins_str += ", "+f;
+            let fixins_str = '';
+            for (let fixin of this.fixins_list.split(';').map(f => TeaFixins[f])) {
+                fixins_str += ', ' + fixin;
             }
-            fixins_str = fixins_str.replace(/^, /, "");
-            if (this.fixins_list.split(";").length > 1) {
-                return fixins_str.substring(0, fixins_str.lastIndexOf(','))+" and"+fixins_str.substring(fixins_str.lastIndexOf(',')+1);
+            fixins_str = fixins_str.replace(/^, /, '');
+            if (this.fixins_list.split(';').length > 1) {
+                return fixins_str.substring(0, fixins_str.lastIndexOf(',')) + ' and' + fixins_str.substring(fixins_str.lastIndexOf(',') + 1);
             } else {
                 return fixins_str;
             }
         }
-        return "";
+        return '';
     }
 
     get datetime(): Date {
-        var datetime = new Date(this.date);
-        var time_str = this.time.toString()
-        datetime.setHours(parseInt(time_str.substring(0, time_str.length-2)));
-        datetime.setMinutes(parseInt(time_str.substring(time_str.length-2)));
+        let datetime = new Date(this.date);
+        let time_str = this.time.toString()
+        datetime.setHours(parseInt(time_str.substring(0, time_str.length - 2), 10));
+        datetime.setMinutes(parseInt(time_str.substring(time_str.length - 2), 10));
         return datetime;
     }
 }
