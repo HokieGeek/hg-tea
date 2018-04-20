bbb8f47e9adebfa36dcb34985bd6726cbc67a226
HokieGeek <andres.f.perez@gmail.com>
2018-04-19 20:48:13 -0400

    Did unit test for teacupimg

--- src/app/journal/teacupimg/teacupimg.component.spec.ts
+++ src/app/journal/teacupimg/teacupimg.component.spec.ts
@@ -1,10 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { TeacupimgComponent } from './teacupimg.component';
 
-describe('TeacupimgComponent', () => {
+describe('TeacupimgComponent Unselected', () => {
   let component: TeacupimgComponent;
   let fixture: ComponentFixture<TeacupimgComponent>;
+  let classes: string[]
+
+  const defaultClass = 'ratingImg';
+  const unselectedClass = 'ratingImgUnselected';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +21,62 @@ describe('TeacupimgComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TeacupimgComponent);
     component = fixture.componentInstance;
+
+    component.unselect = 'true';
+
     fixture.detectChanges();
+
+    classes = fixture.debugElement.query(By.css('img')).nativeElement.classList;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('contains default class', () => {
+    expect(Object.values(classes).includes(defaultClass)).toBeTruthy();
+  });
+
+  it('has Unselected class', () => {
+    expect(Object.values(classes).includes(unselectedClass)).toBeTruthy();
+  });
+});
+
+describe('TeacupimgComponent Selected', () => {
+  let component: TeacupimgComponent;
+  let fixture: ComponentFixture<TeacupimgComponent>;
+  let classes: string[]
+
+  const defaultClass = 'ratingImg';
+  const unselectedClass = 'ratingImgUnselected';
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TeacupimgComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TeacupimgComponent);
+    component = fixture.componentInstance;
+
+    component.unselect = 'false';
+
+    fixture.detectChanges();
+
+    classes = fixture.debugElement.query(By.css('img')).nativeElement.classList;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('contains default class', () => {
+    expect(Object.values(classes).includes(defaultClass)).toBeTruthy();
+  });
+
+  it('does not have Unselected class', () => {
+    expect(!Object.values(classes).includes(unselectedClass)).toBeTruthy();
   });
 });
