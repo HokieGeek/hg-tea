c0798f2cf82d43138b69d88f6a5c47886f06dc32
HokieGeek <andres.f.perez@gmail.com>
2018-04-19 20:48:37 -0400

    Formatting change only

--- src/app/journal/teacupimg/teacupimg.component.spec.ts
+++ src/app/journal/teacupimg/teacupimg.component.spec.ts
@@ -4,79 +4,79 @@ import { By } from '@angular/platform-browser';
 import { TeacupimgComponent } from './teacupimg.component';
 
 describe('TeacupimgComponent Unselected', () => {
-  let component: TeacupimgComponent;
-  let fixture: ComponentFixture<TeacupimgComponent>;
-  let classes: string[]
+    let component: TeacupimgComponent;
+    let fixture: ComponentFixture<TeacupimgComponent>;
+    let classes: string[]
 
-  const defaultClass = 'ratingImg';
-  const unselectedClass = 'ratingImgUnselected';
+    const defaultClass = 'ratingImg';
+    const unselectedClass = 'ratingImgUnselected';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TeacupimgComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ TeacupimgComponent ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TeacupimgComponent);
-    component = fixture.componentInstance;
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TeacupimgComponent);
+        component = fixture.componentInstance;
 
-    component.unselect = 'true';
+        component.unselect = 'true';
 
-    fixture.detectChanges();
+        fixture.detectChanges();
 
-    classes = fixture.debugElement.query(By.css('img')).nativeElement.classList;
-  });
+        classes = fixture.debugElement.query(By.css('img')).nativeElement.classList;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('contains default class', () => {
-    expect(Object.values(classes).includes(defaultClass)).toBeTruthy();
-  });
+    it('contains default class', () => {
+        expect(Object.values(classes).includes(defaultClass)).toBeTruthy();
+    });
 
-  it('has Unselected class', () => {
-    expect(Object.values(classes).includes(unselectedClass)).toBeTruthy();
-  });
+    it('has Unselected class', () => {
+        expect(Object.values(classes).includes(unselectedClass)).toBeTruthy();
+    });
 });
 
 describe('TeacupimgComponent Selected', () => {
-  let component: TeacupimgComponent;
-  let fixture: ComponentFixture<TeacupimgComponent>;
-  let classes: string[]
+    let component: TeacupimgComponent;
+    let fixture: ComponentFixture<TeacupimgComponent>;
+    let classes: string[]
 
-  const defaultClass = 'ratingImg';
-  const unselectedClass = 'ratingImgUnselected';
+    const defaultClass = 'ratingImg';
+    const unselectedClass = 'ratingImgUnselected';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TeacupimgComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ TeacupimgComponent ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TeacupimgComponent);
-    component = fixture.componentInstance;
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TeacupimgComponent);
+        component = fixture.componentInstance;
 
-    component.unselect = 'false';
+        component.unselect = 'false';
 
-    fixture.detectChanges();
+        fixture.detectChanges();
 
-    classes = fixture.debugElement.query(By.css('img')).nativeElement.classList;
-  });
+        classes = fixture.debugElement.query(By.css('img')).nativeElement.classList;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('contains default class', () => {
-    expect(Object.values(classes).includes(defaultClass)).toBeTruthy();
-  });
+    it('contains default class', () => {
+        expect(Object.values(classes).includes(defaultClass)).toBeTruthy();
+    });
 
-  it('does not have Unselected class', () => {
-    expect(!Object.values(classes).includes(unselectedClass)).toBeTruthy();
-  });
+    it('does not have Unselected class', () => {
+        expect(!Object.values(classes).includes(unselectedClass)).toBeTruthy();
+    });
 });
