887c00fd21db7f390e840fa71bf52e9bd6526765
HokieGeek <andres.f.perez@gmail.com>
2018-04-19 21:02:31 -0400

    Formatting change, only

--- src/app/journal/journal-entry.component.spec.ts
+++ src/app/journal/journal-entry.component.spec.ts
@@ -10,70 +10,70 @@ import { Tea } from '../tea'
 import { Entry } from '../entry'
 
 describe('JournalEntryComponent', () => {
-  let component: JournalEntryComponent;
-  let fixture: ComponentFixture<JournalEntryComponent>;
+    let component: JournalEntryComponent;
+    let fixture: ComponentFixture<JournalEntryComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-        imports: [ FormsModule ],
-        declarations: [
-            NaturalLanguageDatePipe,
-            TeacupimgComponent,
-            JournalEntryComponent
-        ],
-        providers: [ NaturalLanguageDatePipe ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ FormsModule ],
+            declarations: [
+                NaturalLanguageDatePipe,
+                TeacupimgComponent,
+                JournalEntryComponent
+            ],
+            providers: [ NaturalLanguageDatePipe ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-      fixture = TestBed.createComponent(JournalEntryComponent);
-      component = fixture.componentInstance;
+    beforeEach(() => {
+        fixture = TestBed.createComponent(JournalEntryComponent);
+        component = fixture.componentInstance;
 
-      component.entry = new Entry(
-          1, // teaId
-          'COMMENT', // comments
-          '12/30/2011 7:49:05', // timestamp
-          '9999/99/99', // date
-          900, // time
-          3, // rating
-          '', // pictures
-          '1m 2s', // steeptime
-          0, // steepingvessel_idx
-          212, // steeptemperature
-          '', // sessioninstance
-          '' // fixins_list
-      )
+        component.entry = new Entry(
+            1, // teaId
+            'COMMENT', // comments
+            '12/30/2011 7:49:05', // timestamp
+            '9999/99/99', // date
+            900, // time
+            3, // rating
+            '', // pictures
+            '1m 2s', // steeptime
+            0, // steepingvessel_idx
+            212, // steeptemperature
+            '', // sessioninstance
+            '' // fixins_list
+        )
 
-      component.tea = new Tea(
-          1, // id
-          'Foobar', // name
-          '12/30/2011 7:49:05', // timestamp
-          '9999/99/99', // date
-          'Sheng', // type
-          'Yunnan', // region
-          2999, // year
-          0, // flush
-          'tea.awesome.site', // purchaselocation
-          '9999/99/99', // purchasedate
-          '99.99', // purchaseprice
-          '', // ratings
-          'COMMENT', // comments
-          [], // pictures
-          'China', // country
-          '', // leafgrade
-          '', // blendedteas
-          '', // blendratio
-          'Full Beeng', // size
-          true , // stocked
-          true , // aging
-          'loose' // packaging
-      )
+        component.tea = new Tea(
+            1, // id
+            'Foobar', // name
+            '12/30/2011 7:49:05', // timestamp
+            '9999/99/99', // date
+            'Sheng', // type
+            'Yunnan', // region
+            2999, // year
+            0, // flush
+            'tea.awesome.site', // purchaselocation
+            '9999/99/99', // purchasedate
+            '99.99', // purchaseprice
+            '', // ratings
+            'COMMENT', // comments
+            [], // pictures
+            'China', // country
+            '', // leafgrade
+            '', // blendedteas
+            '', // blendratio
+            'Full Beeng', // size
+            true , // stocked
+            true , // aging
+            'loose' // packaging
+        )
 
-      fixture.detectChanges();
-  });
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
