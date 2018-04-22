b511c3a865db1210ab1b5d1fde98f5dd2d0315b4
HokieGeek <andres.f.perez@gmail.com>
2018-04-22 08:53:01 -0400

    Continuing to stub out helper functions and simul-fixed the linter errors

--- src/app/test-utils.ts
+++ src/app/test-utils.ts
@@ -3,11 +3,7 @@ export class TestUtils {
     private maxNumFixins = 11;
     private maxNumSteepingVessels = 9;
 
-    /*
-    beforeEach(() => {
-        fixture = TestBed.createComponent(JournalComponent);
-        component = fixture.componentInstance;
-
+    createDummyEntry(): Entry {
         let id = 0;
 
         let now = new Date();
@@ -16,27 +12,44 @@ export class TestUtils {
 
         let fixins: string;
         for (let i = Math.floor(Math.random() * 2); i >= 0; i--) {
-            fixins += Math.floor(Math.random() * maxNumFixins);
+            fixins += Math.floor(Math.random() * this.maxNumFixins);
             fixins += ';';
         }
         fixins = fixins.slice(0, -1);
 
-        component.entries = [new Entry(
+        return new Entry(
             id, // teaId (HAS TO MATCH ARRAY POS)
             'COMMENT', // comments
             '12/30/2011 7:49:05', // timestamp
             today, // date
             time, // time
-            Math.floor(Math.random() * numRatingValues) + 1, // rating
+            Math.floor(Math.random() * this.numRatingValues) + 1, // rating
             '', // pictures
             '1m 2s', // steeptime
-            Math.floor(Math.random() * maxNumSteepingVessels), // steepingvessel_idx
+            Math.floor(Math.random() * this.maxNumSteepingVessels), // steepingvessel_idx
             212, // steeptemperature
             '', // sessioninstance
             fixins // fixins_list
-        )];
+        );
+    }
 
-        component.teas = [new Tea(
+    createDummyTea(): Tea {
+        let id = 0;
+
+        let now = new Date();
+        let today = (now.getMonth() + 1) + '/' + now.getDate() + '/' + now.getFullYear();
+        // let time = parseInt(now.getHours() + '' + now.getMinutes(), 10);
+
+        /*
+        let fixins: string;
+        for (let i = Math.floor(Math.random() * 2); i >= 0; i--) {
+            fixins += Math.floor(Math.random() * maxNumFixins);
+            fixins += ';';
+        }
+        fixins = fixins.slice(0, -1);
+         */
+
+        return new Tea(
             id, // id
             'Foobar', // name
             '12/30/2011 7:49:05', // timestamp
@@ -59,9 +72,6 @@ export class TestUtils {
             true , // stocked
             true , // aging
             'loose' // packaging
-        )];
-
-        fixture.detectChanges();
-    });
-     */
+        );
+    }
 }
