4b44b9de180bc0d76cfa2f156edd16e6e346a96e
HokieGeek <andres.f.perez@gmail.com>
2018-04-24 10:46:24 -0400

    Did a good chunk of testing for the entry and tea classes

---
+++ src/app/entry.spec.ts
@@ -0,0 +1,79 @@
+import { Entry } from './entry';
+
+describe('journal entry', () => {
+    const numRatingValues = 4;
+    const maxNumFixins = 11;
+    const maxNumSteepingVessels = 9;
+
+    const now = new Date();
+    const teaid = 0;
+    const date = (now.getMonth() + 1) + '/' + now.getDate() + '/' + now.getFullYear();
+    const time = parseInt(now.getHours() + '' + now.getMinutes(), 10);
+
+    let fixins: string;
+    for (let i = Math.floor(Math.random() * 2); i >= 0; i--) {
+        fixins += Math.floor(Math.random() * maxNumFixins);
+        fixins += ';';
+    }
+    fixins = fixins.slice(0, -1);
+
+    const comments = 'COMMENT';
+    const timestamp = '12/30/2011 7:49:05';
+    const rating = Math.floor(Math.random() * numRatingValues) + 1;
+    const pictures = '';
+    const steeptime = '1m 2s';
+    const steepingvessel = Math.floor(Math.random() * maxNumSteepingVessels);
+    const steeptemperature = 212;
+    const sessioninstance = '';
+
+    const val = new Entry(
+        teaid, // (HAS TO MATCH ARRAY POS)
+        comments,
+        timestamp,
+        date,
+        time,
+        rating,
+        pictures,
+        steeptime,
+        steepingvessel,
+        steeptemperature,
+        sessioninstance,
+        fixins
+    );
+
+    it('should create', () => {
+        expect(val).toBeTruthy();
+    });
+
+    it('values should match those passed to constructor', () => {
+        expect(val.teaId).toBe(teaid);
+        expect(val.comments).toBe(comments);
+        expect(val.timestamp).toBe(timestamp);
+        expect(val.date).toBe(date);
+        expect(val.time).toBe(time);
+        expect(val.rating).toBe(rating);
+        expect(val.pictures).toBe(pictures);
+        expect(val.steeptime).toBe(steeptime);
+        expect(val.steepingvessel_idx).toBe(steepingvessel);
+        expect(val.steeptemperature).toBe(steeptemperature);
+        expect(val.sessioninstance).toBe(sessioninstance);
+        expect(val.fixins_list).toBe(fixins);
+    });
+
+    xit('check steeping vessel getter returns expected value', () => {
+        // get steepingvessel() { return SteepingVessels[this.steepingvessel_idx]; }
+    });
+
+    xit('check fixins getter returns expected value', () => {
+        // get fixins() {
+    });
+
+    it('check datetime getter returns expected value', () => {
+        const dt = val.datetime;
+        const hasDate = (dt.getMonth() + 1) + '/' + dt.getDate() + '/' + dt.getFullYear();
+        const hasTime = parseInt(dt.getHours() + '' + dt.getMinutes(), 10);
+
+        expect(hasDate).toBe(date);
+        expect(hasTime).toBe(time);
+    });
+});
---
+++ src/app/tea.spec.ts
@@ -0,0 +1,109 @@
+import { Tea } from './tea';
+
+describe('tea entry', () => {
+    const id = Math.floor(Math.random()) + 1;
+
+    const now = new Date();
+    const today = (now.getMonth() + 1) + '/' + now.getDate() + '/' + now.getFullYear();
+
+    const name = 'Foobar';
+    const timestamp = '12/30/2011 7:49:05';
+    const date = today;
+    const teatype = 'Sheng';
+    const region = 'Yunnan';
+    const year = (new Date()).getFullYear();
+    const flush = 0;
+    const purchaseLocation = 'tea.awesome.site';
+    const purchaseDate = today;
+    const purchasePrice = '99.99';
+    const ratings = '';
+    const comments = 'COMMENT';
+    const pictures = [];
+    const country = 'China';
+    const leafgrade = '';
+    const blendedteas = '';
+    const blendratio = '';
+    const size = 'Full Beeng';
+    const stocked = true ;
+    const aging = true ;
+    const packaging = 'loose';
+
+    it('should create', () => {
+        const tea = new Tea(
+            id,
+            name,
+            timestamp,
+            date,
+            teatype,
+            region,
+            year,
+            flush,
+            purchaseLocation,
+            purchaseDate,
+            purchasePrice,
+            ratings,
+            comments,
+            pictures,
+            country,
+            leafgrade,
+            blendedteas,
+            blendratio,
+            size,
+            stocked,
+            aging,
+            packaging
+        );
+
+        expect(tea).toBeTruthy();
+    });
+
+    it('values should match those passed to constructor', () => {
+        const tea = new Tea(
+            id,
+            name,
+            timestamp,
+            date,
+            teatype,
+            region,
+            year,
+            flush,
+            purchaseLocation,
+            purchaseDate,
+            purchasePrice,
+            ratings,
+            comments,
+            pictures,
+            country,
+            leafgrade,
+            blendedteas,
+            blendratio,
+            size,
+            stocked,
+            aging,
+            packaging
+        );
+
+        expect(tea.id).toBe(id);
+        expect(tea.name).toBe(name);
+        expect(tea.timestamp).toBe(timestamp);
+        expect(tea.date).toBe(date);
+        expect(tea.type).toBe(teatype);
+        expect(tea.region).toBe(region);
+        expect(tea.year).toBe(year);
+        expect(tea.flush).toBe(flush);
+        expect(tea.purchaselocation).toBe(purchaseLocation);
+        expect(tea.purchasedate).toBe(purchaseDate);
+        expect(tea.purchaseprice).toBe(purchasePrice);
+        expect(tea.ratings).toBe(ratings);
+        expect(tea.comments).toBe(comments);
+        expect(tea.pictures).toBe(pictures);
+        expect(tea.country).toBe(country);
+        expect(tea.leafgrade).toBe(leafgrade);
+        expect(tea.blendedteas).toBe(blendedteas);
+        expect(tea.blendratio).toBe(blendratio);
+        expect(tea.size).toBe(size);
+        expect(tea.stocked).toBe(stocked);
+        expect(tea.aging).toBe(aging);
+        expect(tea.packaging).toBe(packaging);
+    });
+});
