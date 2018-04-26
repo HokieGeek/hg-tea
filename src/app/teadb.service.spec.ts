0e92141ecef638f3418ead2106eb9d197050772b
HokieGeek <andres.f.perez@gmail.com>
2018-04-25 21:11:23 -0400

    Placeholder for the teadb service test spec

---
+++ src/app/teadb.service.spec.ts
@@ -0,0 +1,8 @@
+// import { async, TestBed } from '@angular/core/testing';
+
+describe('TeaDbService', () => {
+    // TODO: need to mock the Http stuff
+    it('should create', () => {
+        pending('TODO');
+    });
+});
