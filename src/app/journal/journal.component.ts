0e14f70409b3734288ba19d22f6e392859886db7
HokieGeek <andres.f.perez@gmail.com>
2018-05-12 19:50:15 -0400

    I cannot get regular bootstrap collapse to work correctly. Time for an alternative

--- src/app/journal/journal.component.ts
+++ src/app/journal/journal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, Renderer2 } from '@angular/core';
 
 import { Tea } from '../tea';
 import { Entry } from '../entry';
@@ -8,15 +8,48 @@ import { Entry } from '../entry';
     templateUrl: 'journal.component.html',
     styleUrls: ['./journal.component.css']
 })
-export class JournalComponent {
+export class JournalComponent implements OnInit, AfterViewInit {
     @Input() teaId: number;
     @Input() entries: Entry[];
     private expanded = false;
+    private collapseButtonText: string = 'more';
+
+    constructor(private renderer: Renderer2) { }
+
+    ngOnInit() {
+    }
+
+
+    ngAfterViewInit() {
+        /*
+        if (this.entries.length > 1) {
+            // this.collapseButtonText = String(this.entries.length - 1) + ' more';
+            $('#moreEntries-'+this.teaId)
+                .on('shown.bs.collapse', () {
+                    console.log('shown');
+                    this.collapseButtonText = 'less';
+                })
+                .on('hidden.bs.collapse', () {
+                    console.log('hidden');
+                    this.collapseButtonText = String(this.entries.length - 1) + ' more';
+                });
+            // $('#moreEntries-'+this.teaId)
+        }
+        */
+    }
 
     get sortedEntries() {
         if (this.entries) {
             return this.entries.slice().reverse();
         }
         return [];
+    }
+
+    entriesToggle() {
+        $('#moreEntries-'+this.teaId).collapse(this.expanded ? 'hide' : 'show');
+        // const el = this.renderer.selectRootElement('#moreEntries-'+this.teaId);
+        // el.collapse(this.expanded ? 'hide' : 'show');
+        this.expanded = !this.expanded;
+        // console.log(el);
     }
 }
