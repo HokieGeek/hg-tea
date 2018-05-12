035e16e50a689a934ae45d19e92144ab08dc22eb
HokieGeek <andres.f.perez@gmail.com>
2018-05-12 13:55:00 -0400

    Tweaking and linting

--- src/app/database-entry/database-entry.component.ts
+++ src/app/database-entry/database-entry.component.ts
@@ -4,6 +4,8 @@ import { Tea } from '../tea';
 import { Entry } from '../entry';
 import { PurchaseInfoComponent } from '../purchase-info/purchase-info.component';
 
+enum ratingTypeEnum {'AVG', 'MEAN'}
+
 @Component({
     selector: 'hg-database-entry',
     templateUrl: 'database-entry.component.html',
@@ -12,8 +14,29 @@ import { PurchaseInfoComponent } from '../purchase-info/purchase-info.component'
 
 export class DatabaseEntryComponent {
     @Input() tea: Tea;
+    private ratingType: ratingTypeEnum = ratingTypeEnum.AVG;
 
     entriesAvgRating(): number {
-        return 3;
+        let total = 0;
+        for (const entry of this.tea.entries) {
+            total += +entry.rating;
+        }
+        return Math.floor(total / this.tea.entries.length);
+    }
+
+    entriesMeanRating(): number {
+        const ratings: number[] = [];
+        for (const entry of this.tea.entries) {
+            ratings.push(+entry.rating);
+        }
+
+        return ratings[Math.floor(this.tea.entries.length / 2)];
+    }
+
+    entriesRating(): number {
+        switch (this.ratingType) {
+            case ratingTypeEnum.AVG: return this.entriesAvgRating();
+            case ratingTypeEnum.MEAN: return this.entriesMeanRating();
+        }
     }
 }
