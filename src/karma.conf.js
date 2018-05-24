c8f99a36fb00f8d96f3ad605057979ae3e2fa823
HokieGeek <andres.f.perez@gmail.com>
2018-05-23 20:40:56 -0400

    I doubt that this will work...

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,9 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-// const puppeteer = require('puppeteer');
-// process.env.CHROME_BIN = puppeteer.executablePath();
-process.env.CHROME_BIN = '/usr/bin/chromium-browser';
+const puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function (config) {
   config.set({
