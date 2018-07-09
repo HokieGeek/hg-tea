0d63462a09074d219a03521838157e94c7969463
HokieGeek <andres.f.perez@gmail.com>
2018-07-09 15:55:24 -0400

    Formatting fix

--- src/app/input/tea-edit/tea-edit.component.spec.ts
+++ src/app/input/tea-edit/tea-edit.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TeaEditComponent } from './tea-edit.component';
 
 describe('TeaEditComponent', () => {
-  let component: TeaEditComponent;
-  let fixture: ComponentFixture<TeaEditComponent>;
+    let component: TeaEditComponent;
+    let fixture: ComponentFixture<TeaEditComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TeaEditComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ TeaEditComponent ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TeaEditComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TeaEditComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/input/tea-edit/tea-edit.component.ts
+++ src/app/input/tea-edit/tea-edit.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'hg-tea-edit',
-  templateUrl: './tea-edit.component.html',
-  styleUrls: ['./tea-edit.component.css']
+    selector: 'hg-tea-edit',
+    templateUrl: './tea-edit.component.html',
+    styleUrls: ['./tea-edit.component.css']
 })
 export class TeaEditComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
