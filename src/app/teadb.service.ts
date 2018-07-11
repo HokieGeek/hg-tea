c73ad6a0c5dd09ca81d23d26f98a1fbe5696cf18
HokieGeek <andres.f.perez@gmail.com>
2018-07-10 21:14:51 -0400

    Got the teadb service caching the teas

--- src/app/teadb.service.ts
+++ src/app/teadb.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { map, tap, filter, catchError, pluck } from 'rxjs/operators';
 
 import { environment } from '../environments/environment';
 
@@ -13,34 +13,60 @@ export class TeaDbService {
     private allTeasEndpoint = 'teas';
     private teaEndpoint = 'tea';
 
+    private teasSum = '';
+    private _cachedTeas: Tea[] = [];
+
+    set cachedTeas(c: Tea[]) {
+        // console.log('CACHING: ', c);
+        this._cachedTeas = c;
+    }
+
+    get cachedTeas(): Tea[] {
+        // console.log('Using CACHE: ', this._cachedTeas);
+        return this._cachedTeas;
+    }
+
     constructor (private http: HttpClient) { }
 
     get teasWithEntries(): Observable<Tea[]> {
-        const url = this.host + '/' + this.allTeasEndpoint;
-        return this.http.get<TeaDbEntry[]>(url).pipe(map(teas => teas.map(t => new Tea(t))));
+        return this.http.get<HttpResponse<TeaDbEntry[]>>(
+            this.host + '/' + this.allTeasEndpoint,
+            {   observe: 'response',
+                headers: new HttpHeaders({ 'If-None-Match':  this.teasSum })
+            })
+            .pipe(
+                tap(resp => this.teasSum = resp.headers.get('etag')),
+                filter(resp => resp.status === 200),
+                pluck('body'),
+                map((dbteas: TeaDbEntry[]) => dbteas.map(t => new Tea(t))),
+                tap(teas => this.cachedTeas = teas),
+                catchError(err => {
+                    if (err.status === 304) {
+                        return of(this.cachedTeas);
+                    } else {
+                        return throwError(err);
+                    }
+                })
+            );
     }
 
     createTeaEntry(tea: Tea) {
         const url = this.host + '/' + this.teaEndpoint + '/' + tea.id;
-        this.http.post(url, tea.dbentry).subscribe(res => console.log(res));
-        // TODO: update the entire thing?
+        this.http.post(url, tea.dbentry).subscribe();
     }
 
     updateTeaEntry(tea: Tea) {
         const url = this.host + '/' + this.teaEndpoint + '/' + tea.id;
-        this.http.put(url, tea.dbentry).subscribe(res => console.log(res));
-        // TODO: update the entire thing?
+        this.http.put(url, tea.dbentry).subscribe();
     }
 
     createJournalEntry(tea: Tea, e: Entry) {
         const url = this.host + '/' + this.teaEndpoint + '/' + tea.id + '/entry';
-        this.http.post(url, e.dbentry).subscribe(res => console.log(res));
-        // TODO: update the entire thing?
+        this.http.post(url, e.dbentry).subscribe();
     }
 
     updateJournalEntry(tea: Tea, e: Entry) {
         const url = this.host + '/' + this.teaEndpoint + '/' + tea.id + '/entry';
-        this.http.put(url, e.dbentry).subscribe(res => console.log(res));
-        // TODO: update the entire thing?
+        this.http.put(url, e.dbentry).subscribe();
     }
 }
--- src/app/teas/teas.component.ts
+++ src/app/teas/teas.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Tea } from '../tea';
 import { TeaDbService } from '../teadb.service';
 import { ViewService } from '../view.service';
+
+import { environment } from '../../environments/environment';
 
 import { TestUtils } from '../test-utils';
 
@@ -28,10 +32,22 @@ export class TeasComponent implements OnInit {
     }
 
     ngOnInit() {
-        // this.tea_database = TestUtils.createDummyTeasWithEntries();
-        this.teaDbService.teasWithEntries.subscribe(
-            teas => this.teas = teas,
-            err => this.errorMsg = err
-        );
+        this.updateTeas();
+        // setTimeout(() => { this.updateTeas(); }, 5000);
+    }
+
+    private updateTeas() {
+        // console.log('updateTeas()');
+        this.teaDbService.teasWithEntries
+            .pipe(catchError(err => {
+                    if (!environment.production && (err.status === 404 || err.status === 0)) {
+                        this.teas = TestUtils.createDummyTeasWithEntries();
+                    }
+                    return throwError(err);
+                }))
+            .subscribe(
+                teas => this.teas = teas,
+                err => this.errorMsg = err
+            );
     }
 }
