3e7ab411ddb2218f95076005fdb6dec829a5c359
HokieGeek <andres.f.perez@gmail.com>
2018-07-12 14:26:48 -0400

    Added Name sorter

--- src/app/teas-sidebar/filter/filter.component.ts
+++ src/app/teas-sidebar/filter/filter.component.ts
@@ -31,7 +31,9 @@ export class FilterComponent implements OnInit {
     }
 
     private teaFields(m: (t) => any): string[] {
-        return this.teas.map(m).filter((value, index, self) => self.indexOf(value) === index); // TODO: filter out blanks
+        return this.teas.map(m)
+            .filter((value, index, self) => self.indexOf(value) === index && value.length > 0)
+            .sort((a, b) => a.localeCompare(b));
     }
 
     populateFields() {
